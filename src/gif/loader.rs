2f46dd6cae233d8d43e171ede407f3aac5f35a10
François <francois.mockers@vleue.com>
2024-05-24 23:51:48 +0200

    cleanup

--- src/gif/loader.rs
+++ src/gif/loader.rs
@@ -1,33 +1,62 @@
-use std::io::BufReader;
-
 use bevy::asset::io::Reader;
-use bevy::render::texture::TextureFormatPixelInfo;
-use bevy::utils::{BoxedFuture, HashMap};
+use bevy::asset::{AssetLoader, AsyncReadExt, LoadContext};
+use bevy::utils::BoxedFuture;
 use bevy::{prelude::*, render::render_asset::RenderAssetUsages};
-// use bevy_asset::{AssetLoader, BoxedFuture, Handle, LoadContext, LoadedAsset};
-// use bevy_math::{Rect, Vec2};
-// use bevy_render::{
-//     render_resource::{Extent3d, TextureDimension, TextureFormat},
-//     texture::{Image, TextureFormatPixelInfo},
-// };
-use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
-// use bevy_sprite::TextureAtlas;
-// use bevy_utils::{
-//     tracing::{debug, error},
-//     HashMap,
-// };
-use bevy::asset::{AssetLoader, AsyncReadExt, LoadContext, LoadedAsset};
 
 use image::{codecs::gif::GifDecoder, AnimationDecoder, DynamicImage};
-use rectangle_pack::{
-    contains_smallest_box, pack_rects, volume_heuristic, GroupedRectsToPlace, PackedLocation,
-    RectToInsert, TargetBin,
-};
 
 use super::{AnimatedGif, Frame};
 
+trait SubAssetLoader<A: Asset> {
+    fn add_asset(&mut self, label: String, asset: A) -> Handle<A>;
+}
+
+impl<A: Asset> SubAssetLoader<A> for &mut Assets<A> {
+    fn add_asset(&mut self, _label: String, asset: A) -> Handle<A> {
+        self.add(asset)
+    }
+}
+
+impl<'a, A: Asset> SubAssetLoader<A> for &mut LoadContext<'a> {
+    fn add_asset(&mut self, label: String, asset: A) -> Handle<A> {
+        self.add_labeled_asset(label, asset)
+    }
+}
+
 #[derive(Default)]
 pub struct AnimatedGifLoader;
+
+impl AnimatedGifLoader {
+    fn internal_load(bytes: Vec<u8>, mut images: impl SubAssetLoader<Image>) -> AnimatedGif {
+        let decoder = GifDecoder::new(bytes.as_slice()).unwrap();
+        let frames = decoder.into_frames();
+        let frames_from_gif = frames.collect_frames().expect("error decoding gif");
+
+        let mut frames = vec![];
+        for frame in frames_from_gif.iter() {
+            let image = Image::from_dynamic(
+                DynamicImage::ImageRgba8(frame.buffer().clone()),
+                true,
+                RenderAssetUsages::RENDER_WORLD,
+            );
+            let handle = images.add_asset(format!("frame-{}", frames.len()), image);
+            frames.push(Frame {
+                delay: frame.delay().numer_denom_ms(),
+                image: handle.clone(),
+            });
+        }
+        AnimatedGif { frames }
+    }
+
+    /// For gif that need to be loaded immediately, during app setup.
+    /// This can be useful if the gif is meant to be played as a loading screen.
+    pub fn load_now(path: String, app: &mut App) -> Handle<AnimatedGif> {
+        let mut images = app.world.resource_mut::<Assets<Image>>();
+        let bytes = std::fs::read(path).unwrap();
+        let gif = Self::internal_load(bytes, &mut *images);
+        app.world.resource_mut::<Assets<AnimatedGif>>().add(gif)
+    }
+}
 
 impl AssetLoader for AnimatedGifLoader {
     type Settings = ();
@@ -42,65 +71,8 @@ impl AssetLoader for AnimatedGifLoader {
         Box::pin(async move {
             let mut bytes = Vec::new();
             reader.read_to_end(&mut bytes).await?;
-            // let a = BufReader::new(bytes.as_slice());
-            let decoder = GifDecoder::new(bytes.as_slice()).unwrap();
-            let frames = decoder.into_frames();
-            let frames_from_gif = frames.collect_frames().expect("error decoding gif");
-
-            // let mut images = std::collections::HashMap::default();
-            let mut i = 0;
-            // let mut images = vec![];
-            let mut frames = vec![];
-            // let mut rects_to_place: GroupedRectsToPlace<Handle<Image>> = GroupedRectsToPlace::new();
-            // let mut images_ordered = vec![];
-            // let mut sub = load_context.begin_labeled_asset();
-            for frame in frames_from_gif.iter() {
-                let image = Image::from_dynamic(
-                    DynamicImage::ImageRgba8(frame.buffer().clone()),
-                    true,
-                    RenderAssetUsages::all(),
-                );
-                let handle = load_context.add_labeled_asset(format!("frame{}", i), image);
-                i += 1;
-                // let handle = load_context.set_labeled_asset(
-                //     &format!("frame{}", images.len()),
-                //     LoadedAsset::new(image.clone()),
-                // );
-                // load_context.load(image);
-                // load_context.add_labeled_asset(label, asset)
-
-                // images.insert(handle.clone(), image.clone());
-                frames.push(Frame {
-                    delay: frame.delay().numer_denom_ms(),
-                    image: handle.clone(),
-                });
-                // images_ordered.push(handle.clone());
-                // rects_to_place.push_rect(
-                //     handle,
-                //     None,
-                //     RectToInsert::new(
-                //         image.texture_descriptor.size.width,
-                //         image.texture_descriptor.size.height,
-                //         1,
-                //     ),
-                // );
-            }
-            // load_context.
-            // sub.finish(value, None)
-            // let atlas = build_atlas(rects_to_place, &images, images_ordered, load_context);
-
-            // let atlas_handle = load_context.set_labeled_asset("atlas", LoadedAsset::new(atlas));
-
-            // load_context.set_default_asset(LoadedAsset::new(AnimatedGif {
-            //     frames,
-            //     atlas: atlas_handle,
-            // }));
-
-            // Ok(())
-            Ok(AnimatedGif {
-                frames,
-                // atlas: atlas_handle,
-            })
+            let gif = Self::internal_load(bytes, load_context);
+            Ok(gif)
         })
     }
 
@@ -108,134 +80,3 @@ impl AssetLoader for AnimatedGifLoader {
         &["gif"]
     }
 }
-
-// fn build_atlas(
-//     rects_to_place: GroupedRectsToPlace<Handle<Image>>,
-//     images: &HashMap<Handle<Image>, Image>,
-//     images_ordered: Vec<Handle<Image>>,
-//     load_context: &mut LoadContext,
-// ) -> TextureAtlas {
-//     let mut atlas_texture = Image::default();
-//     let mut current_width = atlas_texture.size().x as u32;
-//     let mut current_height = atlas_texture.size().y as u32;
-//     let mut rect_placements = None;
-
-//     let format = TextureFormat::Rgba8UnormSrgb;
-
-//     while rect_placements.is_none() {
-//         let mut target_bins = std::collections::BTreeMap::new();
-//         target_bins.insert(0, TargetBin::new(current_width, current_height, 1));
-//         rect_placements = match pack_rects(
-//             &rects_to_place,
-//             &mut target_bins,
-//             &volume_heuristic,
-//             &contains_smallest_box,
-//         ) {
-//             Ok(rect_placements) => {
-//                 atlas_texture = Image::new(
-//                     Extent3d {
-//                         width: current_width,
-//                         height: current_height,
-//                         depth_or_array_layers: 1,
-//                     },
-//                     TextureDimension::D2,
-//                     vec![0; format.pixel_size() * (current_width * current_height) as usize],
-//                     format,
-//                     RenderAssetUsages::all(),
-//                 );
-//                 Some(rect_placements)
-//             }
-//             Err(rectangle_pack::RectanglePackError::NotEnoughBinSpace) => {
-//                 current_height *= 2;
-//                 current_width *= 2;
-//                 None
-//             }
-//         };
-//     }
-
-//     let rect_placements = rect_placements.unwrap();
-
-//     let mut texture_rects = vec![
-//         Rect {
-//             min: Vec2::ZERO,
-//             max: Vec2::ZERO
-//         };
-//         rect_placements.packed_locations().len()
-//     ];
-
-//     let mut texture_handles = HashMap::default();
-//     for (texture_handle, (_, packed_location)) in rect_placements.packed_locations().iter() {
-//         let texture = &images.get(texture_handle).unwrap();
-//         let min = Vec2::new(packed_location.x() as f32, packed_location.y() as f32);
-//         let max = min
-//             + Vec2::new(
-//                 packed_location.width() as f32,
-//                 packed_location.height() as f32,
-//             );
-//         texture_handles.insert(texture_handle.clone_weak(), texture_rects.len());
-//         let index = images_ordered
-//             .iter()
-//             .enumerate()
-//             .find(|(_, h)| h == &texture_handle)
-//             .unwrap()
-//             .0;
-//         texture_rects[index] = Rect { min, max };
-//         copy_converted_texture(&mut atlas_texture, format, texture, packed_location);
-//     }
-
-//     let atlas_texture_handle =
-//         load_context.set_labeled_asset("atlas-texture", LoadedAsset::new(atlas_texture.clone()));
-
-//     TextureAtlasLayout {
-//         size: Vec2::new(
-//             atlas_texture.texture_descriptor.size.width as f32,
-//             atlas_texture.texture_descriptor.size.height as f32,
-//         ),
-//         // texture: atlas_texture_handle,
-//         textures: texture_rects,
-//         texture_handles: Some(texture_handles),
-//     }
-// }
-
-// fn copy_texture_to_atlas(
-//     atlas_texture: &mut Image,
-//     texture: &Image,
-//     packed_location: &PackedLocation,
-// ) {
-//     let rect_width = packed_location.width() as usize;
-//     let rect_height = packed_location.height() as usize;
-//     let rect_x = packed_location.x() as usize;
-//     let rect_y = packed_location.y() as usize;
-//     let atlas_width = atlas_texture.texture_descriptor.size.width as usize;
-//     let format_size = atlas_texture.texture_descriptor.format.pixel_size();
-
-//     for (texture_y, bound_y) in (rect_y..rect_y + rect_height).enumerate() {
-//         let begin = (bound_y * atlas_width + rect_x) * format_size;
-//         let end = begin + rect_width * format_size;
-//         let texture_begin = texture_y * rect_width * format_size;
-//         let texture_end = texture_begin + rect_width * format_size;
-//         atlas_texture.data[begin..end].copy_from_slice(&texture.data[texture_begin..texture_end]);
-//     }
-// }
-
-// fn copy_converted_texture(
-//     atlas_texture: &mut Image,
-//     format: TextureFormat,
-//     texture: &Image,
-//     packed_location: &PackedLocation,
-// ) {
-//     if format == texture.texture_descriptor.format {
-//         copy_texture_to_atlas(atlas_texture, texture, packed_location);
-//     } else if let Some(converted_texture) = texture.convert(format) {
-//         debug!(
-//             "Converting texture from '{:?}' to '{:?}'",
-//             texture.texture_descriptor.format, format
-//         );
-//         copy_texture_to_atlas(atlas_texture, &converted_texture, packed_location);
-//     } else {
-//         error!(
-//             "Error converting texture from '{:?}' to '{:?}', ignoring",
-//             texture.texture_descriptor.format, format
-//         );
-//     }
-// }
--- src/gif/mod.rs
+++ src/gif/mod.rs
@@ -1,30 +1,4 @@
-// use bevy_asset::Handle;
-// use bevy_ecs::{
-//     bundle::Bundle,
-//     component::Component,
-//     system::{lifetimeless::SRes, SystemParamItem},
-// };
-// use bevy_math::Vec2;
-// use bevy_reflect::TypeUuid;
-// use bevy_render::{
-//     render_asset::{PrepareAssetError, RenderAsset},
-//     render_resource::TextureViewDescriptor,
-//     renderer::{RenderDevice, RenderQueue},
-//     texture::{DefaultImageSampler, GpuImage, Image, ImageSampler},
-//     view::{ComputedVisibility, Visibility},
-// };
-// use bevy_sprite::{Sprite, TextureAtlas, TextureAtlasSprite};
-// use bevy_time::Timer;
-// use bevy_transform::components::{GlobalTransform, Transform};
-use bevy::{
-    ecs::system::lifetimeless::SRes,
-    prelude::*,
-    render::{
-        render_asset::{PrepareAssetError, RenderAsset, RenderAssetUsages},
-        renderer::{RenderDevice, RenderQueue},
-        texture::{DefaultImageSampler, GpuImage, ImageSampler},
-    },
-};
+use bevy::prelude::*;
 
 pub(crate) mod driver;
 pub(crate) mod loader;
@@ -32,114 +6,7 @@ pub(crate) mod loader;
 #[derive(Clone, Asset, TypePath)]
 pub struct AnimatedGif {
     pub(crate) frames: Vec<Frame>,
-    // pub(crate) atlas: Handle<TextureAtlasLayout>,
 }
-
-// impl RenderAsset for AnimatedGif {
-//     type PreparedAsset = GpuImage;
-//     type Param = (
-//         SRes<RenderDevice>,
-//         SRes<RenderQueue>,
-//         SRes<DefaultImageSampler>,
-//     );
-
-//     fn asset_usage(&self) -> RenderAssetUsages {
-//         // self.asset_usage
-//         RenderAssetUsages::all()
-//     }
-
-//     // type ExtractedAsset = Image;
-
-//     // type PreparedAsset = GpuImage;
-//     // type Param = (
-//     //     SRes<RenderDevice>,
-//     //     SRes<RenderQueue>,
-//     //     SRes<DefaultImageSampler>,
-//     // );
-
-//     // /// Clones the Image.
-//     // fn extract_asset(&self) -> Self::ExtractedAsset {
-//     //     // self.clone()
-//     //     todo!()
-//     // }
-
-//     // /// Converts the extracted image into a [`GpuImage`].
-//     // fn prepare_asset(
-//     //     image: Self::ExtractedAsset,
-//     //     (render_device, render_queue, default_sampler): &mut SystemParamItem<Self::Param>,
-//     // ) -> Result<Self::PreparedAsset, PrepareAssetError<Self::ExtractedAsset>> {
-//     //     let texture = render_device.create_texture_with_data(
-//     //         render_queue,
-//     //         &image.texture_descriptor,
-//     //         &image.data,
-//     //     );
-
-//     //     let texture_view = texture.create_view(
-//     //         image
-//     //             .texture_view_descriptor
-//     //             .or_else(|| Some(TextureViewDescriptor::default()))
-//     //             .as_ref()
-//     //             .unwrap(),
-//     //     );
-//     //     let size = Vec2::new(
-//     //         image.texture_descriptor.size.width as f32,
-//     //         image.texture_descriptor.size.height as f32,
-//     //     );
-//     //     let sampler = match image.sampler_descriptor {
-//     //         ImageSampler::Default => (***default_sampler).clone(),
-//     //         ImageSampler::Descriptor(descriptor) => render_device.create_sampler(&descriptor),
-//     //     };
-
-//     //     Ok(GpuImage {
-//     //         texture,
-//     //         texture_view,
-//     //         texture_format: image.texture_descriptor.format,
-//     //         sampler,
-//     //         size,
-//     //         mip_level_count: image.texture_descriptor.mip_level_count,
-//     //     })
-//     // }
-
-//     /// Converts the extracted image into a [`GpuImage`].
-//     fn prepare_asset(
-//         self,
-//         (render_device, render_queue, default_sampler): &mut SystemParamItem<Self::Param>,
-//     ) -> Result<Self::PreparedAsset, PrepareAssetError<Self>> {
-//         let texture = render_device.create_texture_with_data(
-//             render_queue,
-//             &self.texture_descriptor,
-//             // TODO: Is this correct? Do we need to use `MipMajor` if it's a ktx2 file?
-//             wgpu::util::TextureDataOrder::default(),
-//             &self.data,
-//         );
-
-//         let texture_view = texture.create_view(
-//             self.texture_view_descriptor
-//                 .or_else(|| Some(TextureViewDescriptor::default()))
-//                 .as_ref()
-//                 .unwrap(),
-//         );
-//         let size = Vec2::new(
-//             self.texture_descriptor.size.width as f32,
-//             self.texture_descriptor.size.height as f32,
-//         );
-//         let sampler = match self.sampler {
-//             ImageSampler::Default => (***default_sampler).clone(),
-//             ImageSampler::Descriptor(descriptor) => {
-//                 render_device.create_sampler(&descriptor.as_wgpu())
-//             }
-//         };
-
-//         Ok(GpuImage {
-//             texture,
-//             texture_view,
-//             texture_format: self.texture_descriptor.format,
-//             sampler,
-//             size,
-//             mip_level_count: self.texture_descriptor.mip_level_count,
-//         })
-//     }
-// }
 
 #[derive(Clone, Debug)]
 pub struct Frame {
@@ -147,13 +14,37 @@ pub struct Frame {
     pub(crate) image: Handle<Image>,
 }
 
+/// Component to help control the animation of an [`AnimatedGif`].
 #[derive(Component, Clone, Default)]
-pub struct AnimatedGifTimer(pub(crate) Timer, pub(crate) usize);
+pub struct AnimatedGifController {
+    pub(crate) timer: Timer,
+    pub(crate) play_count: usize,
+    pub(crate) current_frame: usize,
+    pub(crate) frame_count: usize,
+}
+
+impl AnimatedGifController {
+    /// Number of times the animation has looped.
+    pub fn play_count(&self) -> usize {
+        self.play_count
+    }
+    /// Current frame of the animation.
+    pub fn current_frame(&self) -> usize {
+        self.current_frame
+    }
+
+    /// How many frames the animation has.
+    pub fn frame_count(&self) -> usize {
+        self.frame_count
+    }
+}
 
 #[derive(Bundle, Clone, Default)]
 pub struct AnimatedGifImageBundle {
+    /// Handle to the [`AnimatedGif`] asset to be drawn.
     pub animated_gif: Handle<AnimatedGif>,
-    pub timer: AnimatedGifTimer,
+    /// Controller for animation playback, given informations about current progress.
+    pub controller: AnimatedGifController,
     /// Specifies the rendering properties of the sprite, such as color tint and flip.
     pub sprite: Sprite,
     /// The local transform of the sprite, relative to its parent.
@@ -162,21 +53,6 @@ pub struct AnimatedGifImageBundle {
     pub global_transform: GlobalTransform,
     /// A reference-counted handle to the image asset to be drawn.
     pub texture: Handle<Image>,
-    /// User indication of whether an entity is visible
-    pub visibility: Visibility,
-    /// Inherited visibility of an entity.
-    pub inherited_visibility: InheritedVisibility,
-    /// Algorithmically-computed indication of whether an entity is visible and should be extracted for rendering
-    pub view_visibility: ViewVisibility,
-}
-
-#[derive(Bundle, Clone, Default)]
-pub struct AnimatedGifTextureAtlasBundle {
-    pub animated_gif: Handle<AnimatedGif>,
-    pub timer: AnimatedGifTimer,
-    pub texture_atlas: TextureAtlas,
-    pub transform: Transform,
-    pub global_transform: GlobalTransform,
     /// User indication of whether an entity is visible
     pub visibility: Visibility,
     /// Inherited visibility of an entity.
